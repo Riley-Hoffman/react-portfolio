913478a8cf6fa52b5f6fba6253ffdb1d58a0c91f
Riley Hoffman <riley.n.hoffman@gmail.com>
2024-07-15 11:36:00 -0400

    Move schema to index.html

--- src/App.js
+++ src/App.js
@@ -6,28 +6,13 @@ import About from './components/About';
 import Projects from './components/Projects';
 import Skills from './components/Skills';
 import Contact from './components/Contact';
-import riley from './assets/riley.jpg'
 import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 
 function App() { 
-   const articleStructuredData = {
-    "@context": "https://schema.org",
-    "@type": "WebPage",
-    headline: "Riley Hoffman - Web Developer",
-    description: "I'm a front-end developer with a passion for building accessible and responsive web applications. I quickly learn new concepts and love adding to my growing skill set. I am a proactive problem solver who enjoys writing future-proof, understandable code that fosters collaboration with other developers.",
-    image: riley,
-    datePublished: new Date("2024-07-04T09:25:01.340Z").toISOString(),
-    author: {
-      "@type": "Person",
-      name: "Riley Hoffman",
-      url: "https://rileyhoffman.com",
-    },
-  }; 
   return (
     <HelmetProvider>
       <Router>
         <div className="App">
-          <script type="application/ld+json" dangerouslySetInnerHTML={{ __html: JSON.stringify(articleStructuredData), }} />
           <Header />
           <Routes>
             <Route path='/' element={
