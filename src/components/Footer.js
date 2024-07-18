0505777488b953ef47494e317cfa85829a4fe8f8
Riley Hoffman <riley.n.hoffman@gmail.com>
2024-07-18 13:21:13 -0400

    Added hover effect to git link in footer.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,9 +7,9 @@ function Footer() {
         <footer className="gradient-border">
             <h2 className="sr-only">Footer</h2>
             <div className="wrapper flex">
-                <div>Riley Hoffman | Web Developer 
+                <p>Riley Hoffman | Web Developer 
                     <a href="https://github.com/Riley-Hoffman/react-portfolio" target="_blank" rel="noopener noreferrer" aria-label="Site repo on Github"> <FontAwesomeIcon icon={faGithub} /></a>
-                </div>
+                </p>
                 ©{new Date().getFullYear()}
             </div>
         </footer>
