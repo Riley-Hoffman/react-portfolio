a6226b4f2e731546b39ffb174a94b7489245f4e4
Riley Hoffman <riley.n.hoffman@gmail.com>
2024-07-21 11:05:53 -0400

    Adde dimage preload link using helmet instead

--- src/components/About.js
+++ src/components/About.js
@@ -11,20 +11,11 @@ import useScrollingBackground from '../hooks/useScrollingBackground';
 function About() {
     
     const containerRef = useScrollingBackground();
-    useEffect(() => {
-        const link = document.createElement('link');
-        link.rel = 'preload';
-        link.as = 'image';
-        link.href = clouds;
-        link.fetchpriority = 'high';
-        link.as = 'image';
-        link.type = 'image/jpeg';
-        document.head.appendChild(link);
-      }, []);
 
     return (
         <div>
             <Helmet>
+                <link rel="preload" fetchpriority="high" href={clouds} as="image" type="image/jpeg" />
                 <meta property="og:image" content={thumbnail} />
                 <meta name="description" content="I'm a front-end developer with a passion for building accessible and responsive web applications. I quickly learn new concepts and love adding to my growing skill set. I am a proactive problem solver who enjoys writing future-proof, understandable code that fosters collaboration with other developers." />
                 <link rel="canonical" href="https://rileyhoffman.com" />
