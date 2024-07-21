67f89c1c26b77e76042d1ae0f361cfee3fb0ca71
Riley Hoffman <riley.n.hoffman@gmail.com>
2024-07-21 10:22:37 -0400

    Fix file paths

--- src/components/About.js
+++ src/components/About.js
@@ -2,10 +2,10 @@ import { Helmet } from 'react-helmet-async';
 import ParticleGame from './ParticleGame';
 import '../styles/_about.scss';
 import '../styles/_particleGame.scss';
-import usePreloadImages from './usePreloadImages';
+import usePreloadImages from '../hooks/usePreloadImages';
 import thumbnail from '../assets/thumbnail.jpg';
 import riley from '../assets/riley.png';
-import clouds from '../assets/clouds.png';
+import clouds from '../assets/clouds.jpg';
 import useScrollingBackground from '../hooks/useScrollingBackground';
 
 function About() {
