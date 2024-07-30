4c964eccd012e15e5e58ea8f477464ba061e5eee
Riley Hoffman <riley.n.hoffman@gmail.com>
2024-07-30 16:23:16 -0400

    hamburger removed extra spaces in returned html

--- src/components/Hamburger.js
+++ src/components/Hamburger.js
@@ -49,8 +49,8 @@ const Hamburger = ({ expanded }) => {
 
     return (
         <div className="hamburger-box">
-            <button className="closer" onClick={toggleMenu} aria-label="Close Menu" ></button>
-            <button id="hamburger" aria-expanded={isExpanded} aria-label={isExpanded ? 'Close Menu' : 'Open Menu'} onClick={toggleMenu} ref={hamburgerRef} className="hamburger" >
+            <button className="closer" onClick={toggleMenu} aria-label="Close Menu"></button>
+            <button id="hamburger" aria-expanded={isExpanded} aria-label={isExpanded ? 'Close Menu' : 'Open Menu'} onClick={toggleMenu} ref={hamburgerRef} className="hamburger">
                 <span className="line gradient-border"></span>
                 <span className="line gradient-border"></span>
                 <span className="line gradient-border"></span>
