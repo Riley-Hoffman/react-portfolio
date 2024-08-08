bcebfc34b4395a9998f7fb746039a106c9dcc958
Riley Hoffman <riley.n.hoffman@gmail.com>
2024-08-08 15:45:01 -0400

    Removed border-radius on buttons

--- src/components/header/NavListItem.js
+++ src/components/header/NavListItem.js
@@ -5,11 +5,11 @@ function NavListItem({ to, label, hide, isResume, resumePdf }) {
     return (
         <li className="md:inline block">
             {isResume ? (
-                <a className={`w-full inline-block py-3 pr-5 pl-14 tracking-wider whitespace-nowrap button transition-all duration-200 ease-in-out ${hide ? 'hidden' : ''} md:py-2 md:px-4 hover:[&.button]:bg-pink-200 hover:brightness-90`} href={resumePdf} target="_blank" rel="noopener noreferrer">
+                <a className={`w-full inline-block py-3 pr-5 pl-14 tracking-wider whitespace-nowrap ${hide ? 'hidden' : ''} button md:py-2 md:px-4 hover:[&.button]:bg-pink-200 hover:brightness-90`} href={resumePdf} target="_blank" rel="noopener noreferrer">
                     Resume
                 </a>
             ) : (
-                <NavLink className={`w-full inline-block py-3 pr-5 pl-14 tracking-wider whitespace-nowrap button ${hide ? 'hidden' : ''} md:py-2 md:px-4 hover:[&.button]:bg-pink-200 hover:brightness-90 hover:[&.active]:brightness-100 [&.active]:bg-pink-200 [&.active]:text-zinc`} to={to}>
+                <NavLink className={`w-full inline-block py-3 pr-5 pl-14 tracking-wider whitespace-nowrap ${hide ? 'hidden' : ''} button md:py-2 md:px-4 hover:[&.button]:bg-pink-200 hover:brightness-90 hover:[&.active]:brightness-100 [&.active]:bg-pink-200 [&.active]:text-zinc`} to={to}>
                     {label}
                 </NavLink>
             )}
