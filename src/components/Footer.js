c3bb539780a89ee5e142f8ffeebe4891998dee02
Riley Hoffman <riley.n.hoffman@gmail.com>
2024-08-09 17:40:35 -0400

    Adjust min-w min-h on footer git link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,7 +8,7 @@ function Footer() {
             <h2 className="sr-only">Footer</h2>
             <div className="max-w-screen-xl py-7 md:items-center md:justify-between md:flex">
                 <p className="px-3 py-1 m-0 font-urbanist text-base"><span>Riley Hoffman</span> | Web Developer 
-                    <a className="min-w-[16px] inline-block no-underline group" href="https://github.com/Riley-Hoffman/react-portfolio" target="_blank" rel="noopener noreferrer" aria-label="Site repo on Github"> <FontAwesomeIcon className="ml-1 text-3xl md:text-base group-hover:text-purple-200 group-focus:text-purple-200" icon={faGithub} /></a>
+                    <a className="min-w-[34px] min-h-[30px] inline-block no-underline md:min-w-[24px] md:min-h-[20px] group" href="https://github.com/Riley-Hoffman/react-portfolio" target="_blank" rel="noopener noreferrer" aria-label="Site repo on Github"> <FontAwesomeIcon className="ml-1 text-3xl md:text-base group-hover:text-purple-200 group-focus:text-purple-200" icon={faGithub} /></a>
                 </p>
                 <p className="p-0 m-0 mr-auto text-base">
                     <a className="px-3 py-1" href="/accessibility">Accessibility</a>
