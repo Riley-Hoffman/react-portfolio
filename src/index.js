6230795195197b94054b3cbfcea3837409cef462
Riley Hoffman <riley.n.hoffman@gmail.com>
2024-08-11 12:32:23 -0400

    Added google tag manager tracking

--- src/index.js
+++ src/index.js
@@ -3,7 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { BrowserRouter as Router } from 'react-router-dom';
+import TagManager from 'react-gtm-module';
 
+const tagManagerArgs = {
+  gtmId: 'G-STET7NGB4K',
+};
+
+TagManager.initialize(tagManagerArgs);
 
 ReactDOM.render(
   <React.StrictMode>
