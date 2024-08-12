7cd35b20dbaa0004780a045e6cac96c27ff5a77c
Riley Hoffman <riley.n.hoffman@gmail.com>
2024-08-12 08:19:43 -0400

    Home: clouds img: removed top-0 left-0 from starting classlist

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -17,7 +17,7 @@ function Home() {
             document.head.removeChild(preloadLink);
         };
     }, []);
-    
+
     const imageRef = useParallax();
 
 
@@ -30,7 +30,7 @@ function Home() {
                 <link rel="canonical" href="https://rileyhoffman.com" />
             </Helmet>
             <div className="border-b-4 border-solid gradient-border overlay overflow-hidden before:bg-[radial-gradient(rgba(255,255,255,0.743)_0%,_rgba(255,255,255,0.498)_100%),_linear-gradient(-30deg,_rgba(0,247,255,0.08)_0%,_#0000_15%,_#0000_80%,_rgba(0,247,255,0.08)_100%)]">
-                <img className="max-w-none w-[130vw] h-[120vh] absolute top-0 left-0 z-[-1] object-cover" src={clouds} alt="" width="1920" height="1080" fetchpriority="high" ref={imageRef} />
+                <img className="max-w-none w-[130vw] h-[120vh] absolute z-[-1] object-cover" src={clouds} alt="" width="1920" height="1080" fetchpriority="high" ref={imageRef} />
                 <div className="max-w-screen-xl min-h-[calc(100vh-13.313rem)] py-[0.1px]">
                     <div className="m-[18vh_1.25rem_6.625rem_0] py-5 bg-[radial-gradient(ellipse_closest-side_at_50%_50%,_#fff_0%,_transparent)] text-left md:w-3/5 md:translate-y-[1.25rem]">
                         <h1 className="font-semibold m-0 text-3xl leading-normal md:text-4xl md:leading-normal">Riley Hoffman</h1>
