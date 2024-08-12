548d93d47b1d20b1d60619ca229d6c0bab51bd43
Riley Hoffman <riley.n.hoffman@gmail.com>
2024-08-12 15:13:37 -0400

    tailwind.config: diamonds background syntax fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -28,7 +28,7 @@ module.exports = {
         'cut-corners': 'polygon(20% 0%, 80% 0%, 100% 20%, 100% 80%, 80% 100%, 20% 100%, 0% 80%, 0% 20%)',
       },
       backgroundImage: {
-        'diamonds': "url('data:image/svg+xml, %3Csvg id=%27patternId%27 width=%27100%25%27 height=%27100%25%27 xmlns=%27http://www.w3.org/2000/svg%27%3E%3Cdefs%3E%3Cpattern id=%27a%27 patternUnits=%27userSpaceOnUse%27 width=%2750%27 height=%2750%27 patternTransform=%27scale(11) rotate(0)%27%3E%3Crect x=%270%27 y=%270%27 width=%27100%25%27 height=%27100%25%27 fill=%27hsla(0, 0%, 96%, 1)%27/%3E%3Cpath d=%27M50 25L37.5 50 25 25 37.5 0zm-25 0L12.5 50 0 25 12.5 0z%27 stroke-width=%271%27 stroke=%27hsla(247, 46%, 88%, 1)%27 fill=%27none%27/%3E%3C/pattern%3E%3C/defs%3E%3Crect width=%27800%25%27 height=%27800%25%27 transform=%27translate(0,0)%27 fill=%27url(%23a)%27/%3E%3C/svg%3E')",
+        'diamonds': "url('data:image/svg+xml,<svg id=\"patternId\" width=\"100%\" height=\"100%\" xmlns=\"http://www.w3.org/2000/svg\"><defs><pattern id=\"a\" patternUnits=\"userSpaceOnUse\" width=\"50\" height=\"50\" patternTransform=\"scale(11) rotate(0)\"><rect x=\"0\" y=\"0\" width=\"100%\" height=\"100%\" fill=\"hsla(0, 0%, 96%, 1)\"/><path d=\"M50 25L37.5 50 25 25 37.5 0zm-25 0L12.5 50 0 25 12.5 0z\" stroke-width=\"1\" stroke=\"hsla(247, 46%, 88%, 1)\" fill=\"none\"/></pattern></defs><rect width=\"800%\" height=\"800%\" transform=\"translate(0,0)\" fill=\"url(%23a)\"/></svg>')",
       }
     },
   },
