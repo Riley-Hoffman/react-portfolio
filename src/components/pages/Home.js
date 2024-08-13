f84862a738db74fd178b086be30d907a6d09cfed
Riley Hoffman <riley.n.hoffman@gmail.com>
2024-08-13 10:16:49 -0400

    Resize headshot pt2

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -44,7 +44,7 @@ function Home() {
                 <div className="max-w-screen-xl">
                     <div className="items-center md:flex">
                         <div className="md:w-1/3">
-                            <img className="w-96 h-96 max-w-full object-cover border-x-2 clip-path-cut-corners" src={headshot} alt="" width="384" height="452" />
+                            <img className="w-96 h-96 max-w-full object-cover border-x-2 clip-path-cut-corners" src={headshot} alt="" width="450" height="530" />
                         </div>
                         <div className="py-6 my-6 bg-[whitesmoke] border-2 border-[#d6d2ee] md:w-2/3 lg:px-24">
                             <h2>My Journey</h2>
