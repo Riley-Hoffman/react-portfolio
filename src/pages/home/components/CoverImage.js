eff3b0b86477ff5cb9f3822d34a9fac83d1aa389
Riley Hoffman <riley.n.hoffman@gmail.com>
2024-08-16 11:20:09 -0400

    useParallax/CoverImage: better cls fix. pass velocity to parent instead of top/left. Set starting positions with calc.

--- src/pages/home/components/CoverImage.js
+++ src/pages/home/components/CoverImage.js
@@ -2,9 +2,9 @@ import useParallax from '../../../hooks/useParallax';
 
 const CoverImage = ({srcImg, mobileImg, width, height}) => {
     
-  const { imageRef, position } = useParallax();
+  const {imageRef, velocity} = useParallax();
     return (
-      <img className={`max-w-none w-[130vw] h-[120vh] absolute z-[-1] object-cover`} src={srcImg} srcSet={`${mobileImg} 1200w, ${srcImg} 1920w`} sizes="(max-width: 768px) 100vw, 130vw" alt="" width={width} height={height} fetchpriority="high" ref={imageRef} style={{ top: `${position.top}px`, left: `${position.left}px` }} />
+      <img className={`max-w-none w-[130vw] h-[120vh] absolute z-[-1] object-cover`} src={srcImg} srcSet={`${mobileImg} 1200w, ${srcImg} 1920w`} sizes="(max-width: 768px) 100vw, 130vw" alt="" width={width} height={height} fetchpriority="high" ref={imageRef} style={{ top: `calc((${height} * ${velocity}) + ${velocity})px`, left: `calc(${height} * ${velocity})` }} />
     );
 };
 
