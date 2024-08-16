34df672dd71e5d0c7958b99cd1c98f295abf8612
Riley Hoffman <riley.n.hoffman@gmail.com>
2024-08-16 10:35:35 -0400

    Updated pass parallax to pass top and left values to parent component instead of setting directly.

--- src/hooks/useParallax.js
+++ src/hooks/useParallax.js
@@ -3,14 +3,18 @@ import { useEffect, useRef, useState } from 'react';
 const useParallax = (velocity = 0.1) => {
     const imageRef = useRef(null);
     const [scrollPos, setScrollPos] = useState(0);
+    const [position, setPosition] = useState({ top: 0, left: 0 });
 
     useEffect(() => {
         const updateImagePosition = () => {
             if (imageRef.current) {
                 const rect = imageRef.current.getBoundingClientRect();
                 const height = rect.height;
-                imageRef.current.style.left = `-${Math.round((height - scrollPos) * velocity)}px`;
-                imageRef.current.style.top = `-${Math.round((height - scrollPos) * (velocity + 0.1))}px`;
+                console.log(rect.left, rect.top)
+                setPosition({
+                    left: -Math.round((height - scrollPos) * velocity),
+                    top: -Math.round((height - scrollPos) * (velocity + 0.1)),
+                });
             }
         };
 
@@ -38,7 +42,7 @@ const useParallax = (velocity = 0.1) => {
         };
     }, []);;
 
-    return imageRef;
+    return {imageRef, position};
 }
 
 export default useParallax;
--- src/pages/home/components/CoverImage.js
+++ src/pages/home/components/CoverImage.js
@@ -2,9 +2,9 @@ import useParallax from '../../../hooks/useParallax';
 
 const CoverImage = ({srcImg, mobileImg, width, height}) => {
     
-    const imageRef = useParallax();
+  const { imageRef, position } = useParallax();
     return (
-      <img className="max-w-none w-[130vw] h-[120vh] absolute z-[-1] object-cover" src={srcImg} srcSet={`${mobileImg} 1200w, ${srcImg} 1920w`} sizes="(max-width: 768px) 100vw, 130vw" alt="" width={width} height={height} fetchpriority="high" ref={imageRef} />
+      <img className={`max-w-none w-[130vw] h-[120vh] absolute z-[-1] object-cover`} src={srcImg} srcSet={`${mobileImg} 1200w, ${srcImg} 1920w`} sizes="(max-width: 768px) 100vw, 130vw" alt="" width={width} height={height} fetchpriority="high" ref={imageRef} style={{ top: `${position.top}px`, left: `${position.left}px` }} />
     );
 };
 
