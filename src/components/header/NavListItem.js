7e5a3799cb44903378fa873f0cd05fb8036fad6a
Riley Hoffman <riley.n.hoffman@gmail.com>
2024-08-18 09:12:39 -0400

    NavListItem used variable for commonClasses

--- src/components/header/NavListItem.js
+++ src/components/header/NavListItem.js
@@ -2,14 +2,16 @@ import { NavLink } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 function NavListItem({ to, label, hide, isResume, resumePdf }) {
+    const commonClasses = 'w-full inline-block py-3 pr-5 pl-14 tracking-wider whitespace-nowrap button md:py-2 md:px-4 md:inline-block md:w-auto hover:[&.button]:bg-pink-200 hover:brightness-90';
+    const hiddenClass = hide ? 'hidden' : '';    
     return (
         <li className="md:inline block">
             {isResume ? (
-                <a className={`w-full inline-block py-3 pr-5 pl-14 tracking-wider whitespace-nowrap ${hide ? 'hidden' : ''} button md:py-2 md:px-4 md:inline-block md:w-auto hover:[&.button]:bg-pink-200 hover:brightness-90`} href={resumePdf} target="_blank" rel="noopener noreferrer">
+                <a className={`button ${commonClasses} ${hiddenClass}`} href={resumePdf} target="_blank" rel="noopener noreferrer" >
                     Resume
                 </a>
             ) : (
-                <NavLink className={`w-full inline-block py-3 pr-5 pl-14 tracking-wider whitespace-nowrap ${hide ? 'hidden' : ''} button md:py-2 md:px-4 md:inline-block md:w-auto hover:[&.button]:bg-pink-200 hover:brightness-90 hover:[&.active]:brightness-100 [&.active]:bg-pink-200 [&.active]:text-zinc`} to={to}>
+                <NavLink className={`button ${commonClasses} ${hiddenClass} hover:[&.active]:brightness-100 [&.active]:bg-pink-200 [&.active]:text-zinc`} to={to} >
                     {label}
                 </NavLink>
             )}
