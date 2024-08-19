21121cbfd19abd7427528d964d529b45a9b8c086
Riley Hoffman <riley.n.hoffman@gmail.com>
2024-08-19 10:36:52 -0400

    NotFound: styling

--- src/components/navigation/NotFound.js
+++ src/components/navigation/NotFound.js
@@ -6,13 +6,13 @@ function NotFound() {
     return (
         <>
             <h1 className="text-center text-3xl leading-normal bg-[#eee2f3] border-b-2 mb-7 mt-0 py-10 px-5 gradient-border inverted md:text-5xl md:leading-normal contrast-more:bg-white">Page Not Found</h1>
-            <div className="max-w-screen-sm p-[1.875rem_0_25vh] text-center"> 
+            <div className="max-w-screen-md p-[1.875rem_0_25vh] text-center"> 
                 <h2 className="text-4xl">404</h2>
-                <p className="text-xl pb-4">Sorry, the document you requested is not available. Please select one of the available pages below:</p>
-                <ul className="px-4 text-2xl text-left">
+                <p className="max-w-screen-sm text-xl pb-4">Sorry, the document you requested is not available. Please select one of the available pages below:</p>
+                <ul className="max-w-screen-md px-8 text-2xl text-left">
                     {RouteList.map(route => (
-                        <li className="py-3 block" key={route.path}>
-                                <Link className="py-1 px-2 no-underline button" to={route.path}>{`${domain}${route.path}`}</Link>
+                        <li className="py-2 block md:py-3" key={route.path}>
+                                <Link className="py-1 px-2 text-lg no-underline button md:text-2xl" to={route.path}>{`${domain}${route.path}`}</Link>
                         </li>
                     ))}
                 </ul>
